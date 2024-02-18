364a216ea50bbd374256ae4d8abeb8fbe4bf7e8e
Vitsinskyi Yevhen <ipz222_vyev@student.ztu.edu.ua>
2024-02-18 17:09:45 +0200

    console app created, gitignore added

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2;
+
+            Console.WriteLine("перше число:");
+            if (!int.TryParse(Console.ReadLine(), out number1))
+            {
+                Console.WriteLine("некоректний формат числа!");
+                return;
+            }
+
+            Console.WriteLine("друге число:");
+            if (!int.TryParse(Console.ReadLine(), out number2))
+            {
+                Console.WriteLine("некоректний формат числа!");
+                return;
+            }
+
+            int sum = number1 + number2;
+            Console.WriteLine($"нума чисел дорiвнює: {sum}");
+        }
+    }
+}
